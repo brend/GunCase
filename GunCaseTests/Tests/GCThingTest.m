4eb71fa559ac9de0911c83364b36cf73ca0b2524
Philipp Brendel <waldrumpus@gmail.com>
2012-06-10 12:44:29 +0200

    Re-brand files :-)

--- GunCaseTests/Tests/GCThingTest.m
+++ GunCaseTests/Tests/GCThingTest.m
@@ -3,7 +3,7 @@
 //  GunCase
 //
 //  Created by Philipp Brendel on 06.05.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 BrendCorp. All rights reserved.
 //
 
 #import "GCThingTest.h"
