4396ca26f1e37f5d906f6cc83b09eaf736b47a0d
Philipp Brendel <waldrumpus@gmail.com>
2012-06-10 13:34:33 +0200

    Remove camera stuff from director test

--- GunCaseTests/Tests/GCDirectorTest.m
+++ GunCaseTests/Tests/GCDirectorTest.m
@@ -17,12 +17,6 @@
 		*e = [GCDirector sharedDirector];
 	
 	STAssertFalse(d == e, @"Same instance");
-	
-	GCVector *v = [GCVector vectorWithX: 17 y: 4];
-	
-	d.camera = v;
-	
-	STAssertEqualObjects(v, e.camera, @"Cameras differ");
 }
 
 @end
