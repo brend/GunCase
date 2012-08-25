e2305650663b713967ccabfb912dcd9426f89719
Philipp Brendel <waldrumpus@gmail.com>
2012-08-25 11:16:07 +0200

    Mock OpenGL API for Unit Testing
    The Unit Testing project, "GunCaseTests", now has its own prefix header.
    In it, three OpenGL functions have been stubbed, so that the new GCActor
    test can pass: glTranslatef, glRotatef, glScalef.

--- GunCaseTests/Tests/GCActorTest.m
+++ GunCaseTests/Tests/GCActorTest.m
@@ -9,7 +9,21 @@
 #import "GCActorTest.h"
 #import "GCActor.h"
 
+@interface GCActorTest ()
+@property (nonatomic, strong) GCActor *actor;
+@end
+
 @implementation GCActorTest
+
+- (void) setUp
+{
+    self.actor = [[GCActor alloc] init];
+}
+
+- (void) tearDown
+{
+    self.actor = nil;
+}
 
 - (void) testInit
 {
@@ -19,6 +33,11 @@
     STAssertNotNil(a.position, @"Position mustn't be nil");
     STAssertEquals(a.scale, NSMakeSize(1, 1), @"Scale must be 1x1");
     STAssertEquals(a.rotation, 0.0f, @"Rotation must be 0");
+}
+
+- (void) testRender
+{
+    [self.actor render];
 }
 
 @end
