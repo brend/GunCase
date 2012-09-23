730c34fb74b34f307c627a253a6ddd0acd38ed40
Philipp Brendel <waldrumpus@gmail.com>
2012-09-23 14:44:08 +0200

    Add keyboard constants
    Arrow keys, enter, escape...

---
+++ GunCase/GCKeyboardKeys.h
@@ -0,0 +1,19 @@
+//
+//  GCKeyboardKeys.h
+//  GunCase
+//
+//  Created by Philipp Brendel on 23.09.12.
+//
+//
+
+#ifndef GunCase_GCKeyboardKeys_h
+#define GunCase_GCKeyboardKeys_h
+
+#define GCKeyboardKeyLeft   123
+#define GCKeyboardKeyRight  124
+#define GCKeyboardKeyDown   125
+#define GCKeyboardKeyUp     126
+#define GCKeyboardKeyEnter   36
+#define GCKeyboardKeyEscape  53
+
+#endif
